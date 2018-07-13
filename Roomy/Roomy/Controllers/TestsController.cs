0d6717e96fe84873471501b093a29166da9b273b
nabil <abilaz24@gmail.com>
2018-07-13 16:48:56 +0200

    Création Projet Roomy

---
+++ Roomy/Roomy/Controllers/TestsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<string> prenom = new List<string>
+            {
+                "gary", "antoine", "thomas"
+            };
+            return View(prenom);
+        }
+    }
+}
