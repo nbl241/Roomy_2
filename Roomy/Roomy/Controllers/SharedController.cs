258c6e3768dc9cdf18bb84b13e23d24cec0a71e9
Nabil BELLIL <abilaz24@gmail.com>
2018-07-24 15:59:55 +0200

    Création de vue partielle (_TopFive et _DispalyMessage)

---
+++ Roomy/Roomy/Controllers/SharedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class SharedController : BaseController
+    {
+        // GET: Shared
+        [ChildActionOnly]
+        public ActionResult TopFive()
+        {
+            var rooms = db.Rooms.OrderByDescending(x => x.Price).Take(5);
+            return View("_TopFive", rooms);
+        }
+    }
+}
